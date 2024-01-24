0f27a941a7b582387689a527ead12ca013e89caa
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-01-24 23:01:44 +0200

    Implement slider feature, add card section, and bug fixes.
    In this commit, a slider component has been added along with the styling and javascript required for its functioning. Also, a brand new 'cards' section has been created in the user interface with appropriate styles. Minor bug fixes have been implemented for improved UI experience, like correcting spellings and adjusting border properties. Lastly, the main.js and style.min.css have been updated to accommodate these new changes.

--- app/js/main.js
+++ app/js/main.js
@@ -12,20 +12,51 @@
 })();
 
 (() => {
-    const tabsitem = document.querySelectorAll('.tabs__links-item')
+    const tabsItem = document.querySelectorAll('.tabs__links-item')
     const tabsBlocks = document.querySelectorAll('.tabs__content-item')
 
-    tabsitem.forEach((tab, index) => {
+    tabsItem.forEach((tab, index) => {
         tab.addEventListener('click', () => {
 
             tabsBlocks.forEach((content) => {
                 content.classList.remove('active')
             })
-            tabsitem.forEach((content) => {
+            tabsItem.forEach((content) => {
                 content.classList.remove('active')
             })
-            tabsitem[index].classList.add('active')
+            tabsItem[index].classList.add('active')
             tabsBlocks[index].classList.add('active')
         })
     })
 })();
+
+(() => {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        spaceBetween: 25,
+        speed: 400,
+        grabCursor: true,
+
+        // effect: 'fade',
+        // fadeEffect: {
+        // 	crossFade: true
+        // },
+
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    });
+})();
+
+const formPopup = new Popup({
+    id: "form-class",
+    title: "My First Popup",
+    content: `<form class="form__test"><input type="email"><input type="password"></form>.`,
+    disableScroll: false
+});
+
+const randomBtn = document.querySelector('.random-btn')
+
+randomBtn.addEventListener('click', () => {
+    formPopup.show()
+})
